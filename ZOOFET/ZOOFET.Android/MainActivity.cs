5cf2d548d8d43f1eb7ac9f8b3667c4c430dd1878
jhosepvarg <53825224+jhosepvarg@users.noreply.github.com>
2020-04-15 23:51:56 -0500

    F por Karla

--- ZOOFET/ZOOFET.Android/MainActivity.cs
+++ ZOOFET/ZOOFET.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace ZOOFET.Droid
 {
-    [Activity(Label = "ZOOFET", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ZOOFET", Icon = "@drawable/icono", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
